b637e6392c019d6bd658612ad9d7e9861d406f56
Justin Jia <justin.jia.developer@gmail.com>
2016-01-29 13:33:48 -0800

    Fix error in Question 2 R code

--- Homework 2/Hwk2.2 ethan.R
+++ Homework 2/Hwk2.2 ethan.R
@@ -1,23 +1,20 @@
 parta <- function(nreps)
 {
-  L1 <- 0 #B1
-  B2 <- 0
-  A2 <- 0
-  L2 <- 0
+	L1 <- 0 #B1
+	B2 <- 0
+	A2 <- 0
+	L2 <- 0
 
-    L1 <- sample(0:2, nreps, prob = c(0.5,0.4,0.1),replace = TRUE)
+	L1 <- sample(0:2, nreps, prob = c(0.5,0.4,0.1), replace = TRUE)
 
-for(n in 1:nreps)
-{
-    for(k in 1:L1[nreps])
-    {
-      A2[k] <- A2[k] + sample(0:1, 1, prob = c(0.8,0.2))
-    }
-}
+	for(n in 1:nreps)
+	{
+		A2[n] <- sum(sample(0:1, L1[n], prob = c(0.8,0.2), replace = TRUE))
+	}
 
-    B2 <- sample(0:2, nreps, prob = c(0.5,0.4,0.1),replace = TRUE)
+    B2 <- sample(0:2, nreps, prob = c(0.5,0.4,0.1), replace = TRUE)
 
     L2 <- L1 + B2 - A2
 
-  return (cov(L1,L2))
+    return (cov(L1,L2))
 }
