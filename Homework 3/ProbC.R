f8bac6f78c3cbe49552d924f1daa96b493249678
Yangzihao Wang <yzhwang@ucdavis.edu>
2016-02-04 20:52:43 -0800

    Added ProbC.R

---
+++ Homework 3/ProbC.R
@@ -0,0 +1,8 @@
+getnewpass <- function(len) return(sample(0:2,len,replace=TRUE,prob=c(0.5,0.4,0.1)))
+
+sim <- function(nreps) {
+    b <- getnewpass(nreps)
+    b <- b + getnewpass(nreps)
+    b <- b + getnewpass(nreps)
+    sapply(c(0:6), function(x) sum(b==x)/nreps)
+}
