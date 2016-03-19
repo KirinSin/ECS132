87fcaadc232d9ab84ded5e5912efe0c9dcdad9d4
yzhwang <yzhwang@ucdavis.edu>
2016-03-18 22:08:28 -0700

    Added table load code for wordbank data.

---
+++ Project/WB.R
@@ -0,0 +1,33 @@
+library(ggplot2)
+
+loadtable <- function() {
+
+    # read in data from CSV file
+    d<-read.csv("vocabulary_norms_data.csv", fill=TRUE)
+
+    # only extract necessary information for our regression analysis
+    dd<-data.frame(d$data_id, d$age, d$birth_order, d$ethnicity, d$sex, d$mom_ed, d$vocab)
+
+    # form dummy variables for gender and ethnicity
+    dd$d.sex<-as.integer(dd$d.sex=="Male")
+    dd$asian<-as.integer(dd$d.ethnicity=="Asian")
+    dd$black<-as.integer(dd$d.ethnicity=="Black")
+    dd$hispanic<-as.integer(dd$d.ethnicity=="Hispanic")
+    dd$d.ethnicity<-as.integer(dd$d.ethnicity=="White")
+
+    # convert categorical variable to numerical variable
+    ed_levels<-c("Primary", "Some Secondary", "Secondary", "Some College", "College", "Some Graduate", "Graduate")
+    dd$d.mom_ed <- sapply(dd$d.mom_ed, function(x) which(ed_levels==x)[1])
+    bo_levels<-c("First", "Second", "Third", "Fourth", "Fifth", "Sixth", "Seventh", "Eighth")
+    dd$d.birth_order <- sapply(dd$d.birth_order, function(x) which(bo_levels==x)[1])
+
+    # standardize column names
+    colnames(dd)<-c("id", "age", "bo", "white", "gender", "momed", "vocab", "asian", "black", "hispanic")
+
+    # voila, we got a data
+    return(dd)
+}
+
+predict <- function(data) {
+
+}
